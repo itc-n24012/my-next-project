9005c2264d0a5ee19c3868d9e2ef579c9de45596
itc-n24012 <n24012@std.it-college.ac.jp>
2025-01-21 05:36:13 +0000

    カテゴリーページ作成

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -1,8 +1,16 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
 
-export default async function Page() {
-  const { contents: news } = await getNewsList();
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const { contents: news } = await getNewsList({
+    filters: `category[equals]${params.id}`,
+  });
 
   return <NewsList news={news} />;
 }
