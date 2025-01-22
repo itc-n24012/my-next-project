6d4f3ac08671cb47e08e7b5ad4db9a14d98f7f40
itc-n24012 <n24012@std.it-college.ac.jp>
2025-01-22 06:52:34 +0000

    修正

--- app/news/search/page.tsx
+++ app/news/search/page.tsx
@@ -2,7 +2,6 @@ import { getNewsList } from "@/app/_libs/microcms";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import SearchField from "@/app/_components/SearchField";
-import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
 type Props = {
   searchParams: {
@@ -10,7 +9,7 @@ type Props = {
   };
 };
 
-export default async function page({ searchParams }: Props) {
+export default async function Page({ searchParams }: Props) {
   const { contents: news } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
     q: searchParams.q,
