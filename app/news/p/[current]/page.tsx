eadd367c19c4beaa52c8ddf81bbf3c91f68363de
itc-n24012 <n24012@std.it-college.ac.jp>
2025-01-22 06:52:24 +0000

    修正

--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -11,11 +11,12 @@ type Props = {
 };
 
 export default async function Page({ params }: Props) {
-  const current = parseInt(params.current, 10);
+  const current = parseInt(params.current as string, 10);
 
   if (Number.isNaN(current) || current < 1) {
     notFound();
   }
+
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
     offset: NEWS_LIST_LIMIT * (current - 1),
